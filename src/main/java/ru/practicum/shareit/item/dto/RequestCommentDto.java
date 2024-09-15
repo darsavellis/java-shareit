ead30b69c7cd5ccc284d682bf611164f9b3e1c9d
Aleksandr Darsavelidze <darsavellis@yandex.ru>
2024-09-16 00:15:00 +0300

    Реализовать функционал работы с моделями Booking, Comment. Реализовать хранение данных через репозитории используя JPA

--- src/main/java/ru/practicum/shareit/item/dto/RequestCommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/RequestCommentDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Data;
